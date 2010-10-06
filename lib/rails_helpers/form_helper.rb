2242c03db8522df34b78c2200cd90a8a628e6c9b
Jake <github@jake.otherinbox.com>
2010-10-06 14:23:56 -0700

    Added missing default arg values to some helpers.  Also handling wrapped fields with method missing now.

--- lib/rails_helpers/form_helper.rb
+++ lib/rails_helpers/form_helper.rb
@@ -50,14 +50,14 @@ module RailsHelpers::FormHelper
 #		end
 	end
 
-	def sex_select(object_name, method, options, html_options)
+	def sex_select(object_name, method, options={}, html_options={})
 		select(object_name, method,
 			[['male','M'],['female','F']],
 			options, html_options)
 	end
 	alias_method :gender_select, :sex_select
 
-	def _wrapped_sex_select(object_name, method, options, html_options)
+	def _wrapped_sex_select(object_name, method, options={}, html_options={})
 #		field_wrapper(method) do
 			s =  label( object_name, method, options.delete(:label_text) )
 			s << sex_select( object_name, method, options, html_options )
@@ -66,7 +66,7 @@ module RailsHelpers::FormHelper
 #	alias_method :wrapped_gender_select, :wrapped_sex_select
 	alias_method :_wrapped_gender_select, :_wrapped_sex_select
 
-	def _wrapped_select(object_name, method, choices, options, html_options)
+	def _wrapped_select(object_name, method, choices, options={}, html_options={})
 #		field_wrapper(method) do
 			s =  label( object_name, method, options.delete(:label_text) )
 			s << select( object_name, method, choices, options, html_options )
@@ -120,13 +120,13 @@ module RailsHelpers::FormHelper
 #		end
 	end
 
-	def y_n_dk_select(object_name, method, options, html_options)
+	def y_n_dk_select(object_name, method, options={}, html_options={})
 		select(object_name, method,
 			[['Yes',1],['No',2],["Don't Know",999]],
 			options, html_options)
 	end
 
-	def _wrapped_y_n_dk_select(object_name, method, options, html_options)
+	def _wrapped_y_n_dk_select(object_name, method, options={}, html_options={})
 #		field_wrapper(method) do
 			s =  label( object_name, method, options.delete(:label_text) )
 			s << y_n_dk_select( object_name, method, options, html_options )
@@ -177,31 +177,31 @@ ActionView::Base.send(:include, RailsHelpers::FormHelper)
 
 ActionView::Helpers::FormBuilder.class_eval do
 
-	def wrapped_collection_select(method, collection,value_method,
-		text_method,options={},html_options={}, &block)
-		@template.wrapped_collection_select(
-			@object_name, method, collection,value_method,text_method,
-				objectify_options(options),
-				html_options, &block)
-	end
+#	def wrapped_collection_select(method, collection,value_method,
+#		text_method,options={},html_options={}, &block)
+#		@template.wrapped_collection_select(
+#			@object_name, method, collection,value_method,text_method,
+#				objectify_options(options),
+#				html_options, &block)
+#	end
 
-	def wrapped_grouped_collection_select(method, 
-		collection, group_method, group_label_method,
-		option_key_method, option_value_method,
-		options={},html_options={},
-		&block)
-		@template.wrapped_grouped_collection_select(
-			@object_name, method, collection,
-				group_method, group_label_method,
-				option_key_method, option_value_method,
-				objectify_options(options),
-				html_options, &block)
-	end
+#	def wrapped_grouped_collection_select(method, 
+#		collection, group_method, group_label_method,
+#		option_key_method, option_value_method,
+#		options={},html_options={},
+#		&block)
+#		@template.wrapped_grouped_collection_select(
+#			@object_name, method, collection,
+#				group_method, group_label_method,
+#				option_key_method, option_value_method,
+#				objectify_options(options),
+#				html_options, &block)
+#	end
 
-	def wrapped_check_box(method, options = {}, &block)
-		@template.wrapped_check_box( @object_name, 
-			method, objectify_options(options), &block)
-	end
+#	def wrapped_check_box(method, options = {}, &block)
+#		@template.wrapped_check_box( @object_name, 
+#			method, objectify_options(options), &block)
+#	end
 
 	def sex_select(method,options={},html_options={})
 		@template.sex_select(
@@ -211,42 +211,42 @@ ActionView::Helpers::FormBuilder.class_eval do
 	end
 	alias_method :gender_select, :sex_select
 
-	def wrapped_select(method,choices,options={},
-		html_options={}, &block)
-		@template.wrapped_select(
-			@object_name, method, choices,
-				objectify_options(options),
-				html_options, &block)
-	end
+#	def wrapped_select(method,choices,options={},
+#		html_options={}, &block)
+#		@template.wrapped_select(
+#			@object_name, method, choices,
+#				objectify_options(options),
+#				html_options, &block)
+#	end
 
-	def wrapped_sex_select(method,options={},
-		html_options={}, &block)
-		@template.wrapped_sex_select(
-			@object_name, method, 
-				objectify_options(options),
-				html_options, &block)
-	end
-	alias_method :wrapped_gender_select, :wrapped_sex_select
+#	def wrapped_sex_select(method,options={},
+#		html_options={}, &block)
+#		@template.wrapped_sex_select(
+#			@object_name, method, 
+#				objectify_options(options),
+#				html_options, &block)
+#	end
+#	alias_method :wrapped_gender_select, :wrapped_sex_select
 
-	def wrapped_text_area(method, options = {}, &block)
-		@template.wrapped_text_area( @object_name, 
-			method, objectify_options(options), &block)
-	end
+#	def wrapped_text_area(method, options = {}, &block)
+#		@template.wrapped_text_area( @object_name, 
+#			method, objectify_options(options), &block)
+#	end
 
-	def wrapped_text_field(method, options = {}, &block)
-		@template.wrapped_text_field( @object_name, 
-			method, objectify_options(options), &block)
-	end
+#	def wrapped_text_field(method, options = {}, &block)
+#		@template.wrapped_text_field( @object_name, 
+#			method, objectify_options(options), &block)
+#	end
 
 	def date_text_field(method, options = {})
 		@template.date_text_field(
 			@object_name, method, objectify_options(options))
 	end
 
-	def wrapped_date_text_field(method, options = {}, &block)
-		@template.wrapped_date_text_field( @object_name, 
-			method, objectify_options(options), &block)
-	end
+#	def wrapped_date_text_field(method, options = {}, &block)
+#		@template.wrapped_date_text_field( @object_name, 
+#			method, objectify_options(options), &block)
+#	end
 
 	def y_n_dk_select(method,options={},html_options={})
 		@template.y_n_dk_select(
@@ -255,11 +255,35 @@ ActionView::Helpers::FormBuilder.class_eval do
 				html_options)
 	end
 
-	def wrapped_y_n_dk_select(method,options={},html_options={},&block)
-		@template.wrapped_y_n_dk_select(
-			@object_name, method, 
-				objectify_options(options),
-				html_options,&block)
+#	def wrapped_y_n_dk_select(method,options={},html_options={},&block)
+#		@template.wrapped_y_n_dk_select(
+#			@object_name, method, 
+#				objectify_options(options),
+#				html_options,&block)
+#	end
+
+
+
+	def method_missing_with_wrapping(symb,*args, &block)
+		method_name = symb.to_s
+		if method_name =~ /^wrapped_(.+)$/
+#puts method_name
+#puts args.inspect
+#puts '-'
+			i = args.find_index{|i| i.is_a?(Hash) }
+			if i.nil?
+				args.push(objectify_options({}))
+			else
+				args[i] = objectify_options(args[i]) 	
+			end
+#puts args.inspect
+#puts '------'
+			@template.send(method_name,@object_name,*args,&block)
+		else
+			method_missing_without_wrapping(symb,*args, &block)
+		end
 	end
+
+	alias_method_chain :method_missing, :wrapping
 
 end
